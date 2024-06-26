24b49d2c014684b267e4cc7d2c372ae94781824f
stfDS <dossantos.stefane@gmail.com>
2024-06-26 12:36:46 +0300

    message pending

--- routes/Message.js
+++ routes/Message.js
@@ -4,14 +4,19 @@ const messageController = require("../controllers/message.controllers");
 const isAuthenticated = require("../middlewares/isAuth");
 const {
   sendMessage,
-  getMessages
+  getMessages,
 } = require("../controllers/message.controllers");
 
 messageRouter.post("/sendMessage", (req, res) => {
-  sendMessage(req, res)
+  sendMessage(req, res);
 });
+
+messageRouter.post("/user/sendMessage", isAuthenticated(), (req, res) => {
+  sendMessage(req, res);
+});
+
 messageRouter.get("/messages", (req, res) => {
-  getMessages(req, res)
+  getMessages(req, res);
 });
 
 module.exports = messageRouter;
