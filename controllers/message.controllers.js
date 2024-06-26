e5d2c13a21cea7700e3cd6daf4342b595f0c5b44
stfDS <dossantos.stefane@gmail.com>
2024-06-27 00:54:49 +0300

    modif message controller

--- controllers/message.controllers.js
+++ controllers/message.controllers.js
@@ -1,15 +1,41 @@
 const Message = require("../models/Message.model");
-const UserMessage = require("../models/MessageByUser.model");
+const MessageByUser = require("../models/MessageByUser.model");
 
 const sendMessage = async (req, res) => {
   try {
     const { email, subject, message } = req.body;
     const newMessage = new Message({ email, subject, message });
     await newMessage.save();
-    res.status(201).json({ message: "Votre message a été envoyé avec succès !" });
+    res
+      .status(201)
+      .json({ message: "Votre message a été envoyé avec succès !" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Une erreur est survenue lors de l'envoi du message." });
+    res
+      .status(500)
+      .json({ message: "Une erreur est survenue lors de l'envoi du message." });
+  }
+};
+
+const sendMessageByUser = async (req, res) => {
+  try {
+    const user = req.user;
+    const { email, subject, message } = req.body;
+    const newMessage = new MessageByUser({
+      email,
+      subject,
+      message,
+      userId: user._id,
+    });
+    await newMessage.save();
+    res
+      .status(201)
+      .json({ message: "Votre message a été envoyé avec succès !" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Une erreur est survenue lors de l'envoi du message." });
   }
 };
 
@@ -21,8 +47,10 @@ const getMessages = async (req, res) => {
     res.status(200).json(allMessages);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Une erreur est survenue lors de la récupération des messages." });
+    res.status(500).json({
+      message: "Une erreur est survenue lors de la récupération des messages.",
+    });
   }
 };
 
-module.exports = { sendMessage, getMessages };
+module.exports = { sendMessage, getMessages, sendMessageByUser };
--- routes/Message.js
+++ routes/Message.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const messageRouter = express.Router();
-const messageController = require("../controllers/message.controllers");
 const isAuthenticated = require("../middlewares/isAuth");
 const {
   sendMessage,
   getMessages,
-} = require("../controllers/message.controllers");
-
-const {
   sendMessageByUser,
-} = require("../controllers/messageByUser.controllers");
+} = require("../controllers/message.controllers");
 
 messageRouter.post("/sendMessage", (req, res) => {
   sendMessage(req, res);
